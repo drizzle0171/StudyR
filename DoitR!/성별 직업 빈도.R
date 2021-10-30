16e672e8ae054cbe13e0231dfae6335755039012
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-10-30 21:44:15 +0900

    DoitR

---
+++ DoitR!/성별 직업 빈도.R
@@ -0,0 +1,26 @@
+#성별 직업 빈도!
+
+#남성 직업 빈도 상위 10개 추출
+
+job_male <- welfare %>% 
+  filter(!is.na(job) & sex =="male") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+
+job_male
+
+#여성 직업 빈도 상위 10개 추출
+
+job_female <- welfare %>% 
+  filter(!is.na(job) & sex =="female") %>% 
+  group_by(job) %>% 
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+
+job_female
+
+ggplot(data = job_male, aes(x=reorder(job, n), y=n)) + geom_col() + coord_flip()
+ggplot(data = job_female, aes(x=reorder(job, n), y=n)) + geom_col() + coord_flip()
